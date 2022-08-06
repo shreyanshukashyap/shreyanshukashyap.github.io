3751c688dd22bcb6b3612339e90f99f2574ba7ea
Shreyanshu Kashyap <88488301+shreyanshukashyap@users.noreply.github.com>
2022-08-06 16:59:39 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var app = express();
+var engines = require('consolidate');
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+app.set('views', './views');
+app.engine('html', engines.mustache);
+app.set('view engine', 'html');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+
+app.get("/", function (req, res) {
+    res.render("../index");
+})
+app.get("/recruit", function (req, res) {
+    res.render("../Form");
+});
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
+})
